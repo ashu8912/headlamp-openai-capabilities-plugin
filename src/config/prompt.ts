929ce27161ab8db3775d985772afa9688463bff9
Ashu Ghildiyal <ashughildiyal@Ashus-Air.guest.corp.microsoft.com>
2023-09-13 17:01:40 +0530

    setup config and prompt for open ai api

---
+++ src/config/prompt.ts
@@ -0,0 +1,3 @@
+export const prompt = {
+    "pod_error": "Hey i have this pod info in json tell me if there is something wrong with it point it out specifically and suggest fixes, try to answer it in an easy language and cut through extra stuff make it minimal The json starts here"
+}
