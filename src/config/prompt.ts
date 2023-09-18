2a195279a0a9394083858e0c78b155024fc949e4
Ashu Ghildiyal <ashughildiyal@Ashus-MacBook-Air.local>
2023-09-18 22:37:28 +0530

    final changes

--- src/config/prompt.ts
+++ src/config/prompt.ts
@@ -1,3 +1,10 @@
 export const prompt = {
-    "pod_error": "Hey i have this pod info in json tell me if there is something wrong with it point it out specifically and suggest fixes, try to answer it in an easy language and cut through extra stuff make it minimal The json starts here"
+    pod_error: `As a skilled technical expert with specialization in Kubernetes and cloud native technologies, your task is to conduct diagnostic procedures on these technologies. Drawing from your deep understanding of Kubernetes and cloud native principles, as well as your troubleshooting experience, you're expected to identify potential issues and provide solutions to address them. For diagnostic process, please perform the following actions: I am attaching the json for a pod below you have to analyze it and see if it has any issues. 
+    Please ensure that issues and explanations are clear enough to be understood by non-technical users, simplifying complex concepts and solutions.
+
+    hey also make sure you don't send broken texts it should always be a complete text
+    `,
+    deployment_metric: `As a skilled technical expert with specialization in Kubernetes and cloud native technologies, Use the current release of k8s and your task is
+    to analyze the prompt and the resource provided. And suggest yamls with the provided resource if it's relevant Also make sure that if there is any provided yaml it should be enclosed in \`\`\`
+    `
 }
